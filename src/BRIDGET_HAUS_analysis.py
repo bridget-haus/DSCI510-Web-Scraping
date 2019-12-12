2b1a3dba6b6bb6b6d3106acf3ddedd7d3e265741
Bridget Haus <bridgethaus@Bridgets-MacBook-Air.local>
2019-12-12 14:50:37 -0800

    adding files

---
+++ src/BRIDGET_HAUS_analysis.py
@@ -0,0 +1,192 @@
+import sys
+import argparse
+import requests
+import json
+from bs4 import BeautifulSoup
+import sqlite3
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+import seaborn as sns
+import numpy as np
+from numpy import mean
+from numpy import std
+from scipy.stats import pearsonr
+
+#Connect to db
+conn = sqlite3.connect('./college.db')
+cur = conn.cursor()
+
+counter_map = []
+user_input = int()
+fields = ['admission_rate', 'sat_overall', 'loan_completion_rate', 'percent_black', 'percent_hispanic', 'median_income']
+colors = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow']
+
+def main():
+    print('What would you like to do? Please select the number of the options below')
+    print('1. Analyze a single school')
+    print('2. Analyze all schools together')
+    main_input = int(input())
+    if main_input == 1:
+        input_function()
+        rank_trend()
+        demographics_trend()
+    elif main_input == 2:
+        correlation__demographics()
+        correlation__tuition()
+    else:
+        print('Invalid input. Please select the number of the choice above')
+
+
+
+def input_function():
+
+    global counter_map
+    global user_input
+
+    print('Please select the number of the school would you like to analyze.')
+    cur.execute('SELECT college.primary_key, college.college_name FROM Rank JOIN College ON Rank.college_primary_key = College.primary_key JOIN Demographics on rank.college_primary_key = demographics.college_primary_key WHERE Rank.year = 2013 GROUP BY college_name ORDER BY college_name')
+    result = cur.fetchall()
+    counter = 1
+    for college in result:
+        print(f'{counter}.', college[1])
+        counter_map.append(college[0])
+        counter += 1
+    while True:
+        try:
+            user_input = int(input())
+            if 0 < user_input < len(counter_map) + 1:
+                break
+            else:
+                print('Invalid input. Please select the number of the school would you like to analyze.')
+        except:
+            print('Invalid input. Please select the number of the school you would like to analyze.')    
+
+def rank_trend():
+
+    global counter_map
+    global user_input
+
+    years = [year for year in range(2013,2020)]
+    xs = np.array(years, dtype=np.float64)
+    cur.execute('SELECT college_name FROM College WHERE primary_key = ?', (counter_map[user_input - 1],))
+    title = cur.fetchall()
+    cur.execute('SELECT rank FROM Rank WHERE college_primary_key = ?', (counter_map[user_input - 1],))
+    y = cur.fetchall()
+    if y[0][0] != None:
+        sns.set()
+        ax = plt.figure().gca()
+        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+        res = [i[0] for i in y]
+        ys = np.array(res, dtype=np.float64)
+        m = best_fit_slope(xs,ys)
+        if round(m, 5) < 0:
+            m_sign = 'negative'
+            m_interpretation = 'As year increases, the US News Rank generally improves'
+        else:
+            m_sign = 'positive'
+            m_interpretation = 'As year increases, the US News Rank generally worsens'
+        plt.plot(xs, ys, color = 'black', linewidth=4)
+        plt.plot(np.unique(xs), np.poly1d(np.polyfit(xs, ys, 1))(np.unique(xs)), linestyle='dashed')
+        plt.ylim(plt.ylim()[::-1])
+        plt.xlabel('Year')
+        plt.ylabel('US News Rank')
+        plt.title(f'{title[0][0]} US News Rank Trend')
+        plt.show(block=True)
+        print(f'The slope of the best fit regression line is a {m_sign} value: {round(m, 5)}')
+def demographics_trend():
+
+    global counter_map
+    global user_input
+
+    years = [year for year in range(2013,2018)]
+    cur.execute('SELECT college_name FROM College WHERE primary_key = ?', (counter_map[user_input - 1],))
+    title = cur.fetchall()
+    xs = np.array(years, dtype=np.float64)
+    for i in range(len(fields)):
+        select_statement = f'select {fields[i]} from Demographics WHERE college_primary_key = ?'
+        cur.execute(select_statement, (counter_map[user_input - 1],))
+        y = cur.fetchall()
+        if y[0][0] != None:
+            field_name = fields[i].title().replace('_', ' ')
+            sns.set()
+            ax = plt.figure().gca()
+            ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+            cur.execute('select median_income from Demographics where year = 2016 and college_primary_key = ?', (counter_map[user_input - 1],))
+            default = cur.fetchall()[0][0]
+            res = [default if i[0] == None else i[0] for i in y]
+            ys = np.array(res, dtype=np.float64)
+            m = best_fit_slope(xs,ys)
+            if m < 0:
+                m_sign = 'negative'
+            else:
+                m_sign = 'positive'
+            plt.plot(xs, ys, color = colors[i], linewidth=4)
+            plt.plot(np.unique(xs), np.poly1d(np.polyfit(xs, ys, 1))(np.unique(xs)), linestyle='dashed')
+            plt.title(f'{title[0][0]} {field_name} Trend')
+            plt.xlabel('Year')
+            plt.ylabel(field_name)
+            plt.show(block=True)
+            print(f'The slope of the best fit regression line is a {m_sign} value: {round(m, 5)}')
+
+def correlation__demographics():
+
+    fields = ['admission_rate', 'sat_overall', 'loan_completion_rate', 'percent_black', 'percent_hispanic', 'median_income']
+    colors = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow']
+    for i in range(len(fields)):
+        rank_select = f'SELECT rank FROM Rank JOIN College ON Rank.college_primary_key = College.primary_key JOIN Demographics on rank.college_primary_key = demographics.college_primary_key AND rank.year = demographics.year WHERE {fields[i]} is not null and rank < 2018'
+        cur.execute(rank_select)    
+        y = cur.fetchall()
+        ys = [float(number[0]) for number in y]
+        fields_select = f'SELECT {fields[i]} FROM Rank JOIN College ON Rank.college_primary_key = College.primary_key JOIN Demographics on rank.college_primary_key = demographics.college_primary_key AND rank.year = demographics.year WHERE {fields[i]} is not null and rank < 2018'
+        cur.execute(fields_select)
+        x = cur.fetchall()
+        xs = [float(number[0]) for number in x]
+        field_name = fields[i].title().replace('_', ' ')
+        plt.scatter(xs, ys, color = colors[i])
+        plt.ylim(plt.ylim()[::-1])
+        plt.plot(np.unique(xs), np.poly1d(np.polyfit(xs, ys, 1))(np.unique(xs)), linestyle='dashed')
+        plt.title(f'{field_name} vs US News Rank')
+        plt.xlabel(field_name)
+        plt.ylabel("US News Rank")
+        plt.show(block=True)
+        print(f'US News Rank: mean={round(mean(ys),3)} stdv={round(std(ys),3)}')
+        print(f'{field_name}: mean={round(mean(xs),3)} stdv={round(std(xs),3)}')
+        corr = pearsonr(xs, ys)
+        print(f'Pearson correlational coefficient: {round(corr[0], 3)}')
+
+def correlation__tuition():
+    fields = ['in_state_tuition', 'out_state_tuition', 'room_board']
+    colors = ['blue', 'green', 'red']
+    for i in range(len(fields)):
+        rank_select = f'SELECT rank FROM Rank JOIN College ON Rank.college_primary_key = College.primary_key JOIN Tuition on rank.college_primary_key = tuition.college_primary_key WHERE NULLIF({fields[i]}, "") IS NOT NULL and rank IS NOT NULL'
+        cur.execute(rank_select)    
+        y = cur.fetchall()
+        ys = [float(number[0]) for number in y]
+        fields_select = f'SELECT {fields[i]} FROM Rank JOIN College ON Rank.college_primary_key = College.primary_key JOIN Tuition on rank.college_primary_key = tuition.college_primary_key WHERE NULLIF({fields[i]}, "") IS NOT NULL and rank IS NOT NULL'
+        cur.execute(fields_select)
+        x = cur.fetchall()
+        xs = [float(number[0][1:].replace(',', '')) for number in x]
+        field_name = fields[i].title().replace('_', ' ')
+        plt.scatter(xs, ys, color = colors[i])
+        plt.ylim(plt.ylim()[::-1])
+        plt.plot(np.unique(xs), np.poly1d(np.polyfit(xs, ys, 1))(np.unique(xs)), linestyle='dashed')
+        plt.title(f'{field_name} vs US News Rank')
+        plt.xlabel(field_name)
+        plt.ylabel("US News Rank")
+        plt.show(block=True)
+        print(f'US News Rank: mean={round(mean(ys),3)} stdv={round(std(ys),3)}')
+        print(f'{field_name}: mean={round(mean(xs),3)} stdv={round(std(xs),3)}')
+        corr = pearsonr(xs, ys)
+        print(f'Pearson correlational coefficient: {round(corr[0], 3)}')
+
+
+def best_fit_slope(xs,ys):
+    m = (((mean(xs)*mean(ys)) - mean(xs*ys)) /
+         ((mean(xs)**2) - mean(xs*xs)))
+    return m
+
+if __name__ == '__main__':
+    main()
+
---
+++ src/BRIDGET_HAUS_hw5.py
@@ -0,0 +1,241 @@
+import sys
+import argparse
+import requests
+import json
+from bs4 import BeautifulSoup
+import sqlite3
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+rank_stats_url = 'https://publicuniversityhonors.com/2016/09/18/average-u-s-news-rankings-for-126-universities-2010-1017/'
+tuition_url = 'https://phillips-scholarship.org/new-applicants/cost-of-college-list/'
+demographic_base_url = 'https://api.data.gov/ed/collegescorecard/v1/schools'
+demographic_api_key = '6sX5SOPjUqItWIRHi5xxUBOc3Hu0SijTazi2oYxp'
+
+conn = sqlite3.connect('college.db')
+cur = conn.cursor()
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--source', help = 'remote or local')
+    args = parser.parse_args()
+    print(args)
+
+    try:
+        if args.source == 'remote':
+            print('Executing scrapers and API requests...')
+            print('This should only take 2-3 minutes...')
+            setup_tables()
+            insert_college_table()
+            insert_tuition_table()
+            insert_rank_table()
+            insert_demographic_table()
+            print('Finished building database!')
+        elif args.source == 'local':
+            print('local')
+        else:
+            print('please enter arguments --source=remote or –-source=local')
+    except Exception as e:
+        print(e)
+
+#helper function to parse websites
+
+def make_soup(url):
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
+    #catch exceptions
+    except requests.exceptions.RequestException as e:
+        print(e)
+    soup = BeautifulSoup(r.content, 'lxml')
+    return soup
+
+#scrapes website with college tuition data. returns zipped list of tuition data
+
+def get_tuition_stats(url):
+    soup = make_soup(url)
+    college_list = []
+    tuition_in_state_list = []
+    tuition_out_state_list = []
+    room_board_list = []
+    main_table = soup.findAll('table', {"id" : "tablepress-25"})[0]
+    main_body = main_table.find('tbody')
+    for college in main_body.findAll('td', {"class": "column-1"}):
+        college = college.text
+        college_list.append(college)
+    for tuition_in_state in main_body.findAll('td', {"class": "column-2"}):
+        tuition_in_state = tuition_in_state.text
+        tuition_in_state_list.append(tuition_in_state)
+    for tuition_out_state in main_body.findAll('td', {"class": "column-3"}):
+        tuition_out_state = tuition_out_state.text
+        tuition_out_state_list.append(tuition_out_state)
+    for room_board in main_body.findAll('td', {"class": "column-4"}):
+        room_board = room_board.text
+        room_board_list.append(room_board)
+    tuition_zip = [list(a) for a in list(zip(college_list, tuition_in_state_list, tuition_out_state_list, room_board_list))]
+    return tuition_zip
+
+#scrapes website with college rank data. returns zipped list of college rank from 2013 - 2020
+
+def get_rank_stats(url):
+    soup = make_soup(url)
+    college_list = []
+    rank_2013_list = []
+    rank_2014_list = []
+    rank_2015_list = []
+    rank_2016_list = []
+    rank_2017_list = []
+    rank_2018_list = []
+    rank_2019_list = []
+    main_table = soup.findAll('table', {"id" : "tablepress-105"})[0]
+    main_body = main_table.find('tbody')
+    for college in main_body.findAll('td', {"class": "column-1"}):
+        if college.text != "":
+            college_list.append(college.text)
+    for rank_2013 in main_body.findAll('td', {"class": "column-2"}):
+        if rank_2013.text != "":
+            rank_2013_list.append(tuple((2013, rank_2013.text)))
+    for rank_2014 in main_body.findAll('td', {"class": "column-3"}):
+        if rank_2014.text != "":
+            rank_2014_list.append(tuple((2014, rank_2014.text)))
+    for rank_2015 in main_body.findAll('td', {"class": "column-4"}):
+        if rank_2015.text != "":
+            rank_2015_list.append(tuple((2015, rank_2015.text)))
+    for rank_2016 in main_body.findAll('td', {"class": "column-5"}):
+        if rank_2016.text != "":
+            rank_2016_list.append(tuple((2016, rank_2016.text)))
+    for rank_2017 in main_body.findAll('td', {"class": "column-6"}):
+        if rank_2017.text != "":
+            rank_2017_list.append(tuple((2017, rank_2017.text)))
+    for rank_2018 in main_body.findAll('td', {"class": "column-7"}):
+        if rank_2018.text != "":
+            rank_2018_list.append(tuple((2018, rank_2018.text)))
+    for rank_2019 in main_body.findAll('td', {"class": "column-8"}):
+        if rank_2019.text != "":
+            rank_2019_list.append(tuple((2019, rank_2019.text)))
+    rank_zip = [list(a) for a in list(zip(college_list, rank_2013_list, rank_2014_list, rank_2015_list, rank_2016_list, rank_2017_list, rank_2018_list, rank_2019_list))]
+    return rank_zip
+
+#creates api query parameters and calls for each college. returns list of demographic features
+
+def get_demographic_stats(api_url):
+
+    rank_data = get_rank_stats(rank_stats_url)
+
+    years = list(range(2013, 2017))
+    json_response_list = []
+    admission_rate = ''
+    loan_completion_rate = ''
+    sat_score = ''
+    percent_black = ''
+    percent_hispanic = ''
+    median_income = ''
+    for year in years:
+        admission_rate = str(year) + '.admissions.admission_rate.overall,' + admission_rate
+        loan_completion_rate = str(year) + '.completion.completion_rate_4yr_150nt,' + loan_completion_rate
+        sat_score = str(year) + '.admissions.sat_scores.average.overall,' + sat_score
+        percent_black = str(year) + '.student.demographics.race_ethnicity.black,' + percent_black
+        percent_hispanic = str(year) + '.student.demographics.race_ethnicity.hispanic,' + percent_hispanic
+        median_income = str(year) + '.student.demographics.median_family_income,' + median_income
+    fields_base = '?fields=school.name,' + admission_rate + loan_completion_rate + sat_score + percent_black + percent_hispanic + median_income
+    demographic_url = api_url + fields_base
+    for college in rank_data:
+        demographic_params = {'school.name': college[0].replace(',',''), 'api_key': demographic_api_key}
+        json_response = requests.get(demographic_url, params=demographic_params)
+        json_response_list.append(json_response.json())
+    return json_response_list
+
+#maps college name from one data source to another
+
+def fuzzy_wuzzy_mapping(original_data):
+
+    tuition_data = get_tuition_stats(tuition_url)
+
+    mapping_list = []
+    for i in range(len(tuition_data)):
+        mapping_list.append(tuition_data[i][0])
+
+    for i in range(len(original_data)):
+        original_data[i][0] = process.extract(original_data[i][0], mapping_list, limit=1)[0][0]
+    return original_data
+
+#creates SQL tables
+
+def setup_tables():
+
+    cur.execute('DROP TABLE IF EXISTS College')
+    cur.execute('DROP TABLE IF EXISTS Tuition')
+    cur.execute('DROP TABLE IF EXISTS Rank')
+    cur.execute('DROP TABLE IF EXISTS Demographics')
+
+    cur.execute('CREATE TABLE College (primary_key INTEGER PRIMARY KEY AUTOINCREMENT, college_name TEXT)')
+    cur.execute('CREATE TABLE Tuition (primary_key INTEGER PRIMARY KEY AUTOINCREMENT, in_state_tuition INTEGER, out_state_tuition INTEGER, room_board INTEGER, college_primary_key INTEGER)')
+    cur.execute('CREATE TABLE Rank (primary_key INTEGER PRIMARY KEY AUTOINCREMENT, year INTEGER, rank INTEGER, college_primary_key INTEGER)')
+    cur.execute('CREATE TABLE Demographics (primary_key INTEGER PRIMARY KEY AUTOINCREMENT, year INTEGER, sat_overall INTEGER, admission_rate INTEGER, loan_completion_rate INTEGER, percent_black INTEGER, percent_hispanic INTEGER, median_income INTEGER, college_primary_key INTEGER)')
+
+    conn.commit()
+
+#inserts data from scrapers and API requests into each table
+
+def insert_college_table():
+
+    tuition_data = get_tuition_stats(tuition_url)
+
+    for i in range(len(tuition_data)):
+        college_name = tuition_data[i][0]
+        cur.execute("INSERT INTO College (primary_key, college_name) VALUES (?, ?)", (None, college_name))
+
+def insert_tuition_table():
+    
+    tuition_data = get_tuition_stats(tuition_url)
+
+    for i in range(len(tuition_data)):
+        cur.execute("SELECT primary_key from College WHERE college_name = ?", (tuition_data[i][0],))
+        college_primary_key = cur.fetchall()[0][0]
+        in_state_tuition = tuition_data[i][1]
+        out_state_tuition = tuition_data[i][2]
+        room_board = tuition_data[i][3]
+        cur.execute('INSERT INTO Tuition (primary_key, in_state_tuition, out_state_tuition, room_board, college_primary_key) VALUES ( ?, ?, ?, ?, ? )', (None, in_state_tuition, out_state_tuition, room_board, college_primary_key))
+
+def insert_rank_table():
+
+    rank_data = get_rank_stats(rank_stats_url)
+    rank_map_data = fuzzy_wuzzy_mapping(rank_data)
+
+    for i in range(len(rank_map_data)):
+        cur.execute("SELECT primary_key from College WHERE college_name = ?", (rank_map_data[i][0],))
+        college_primary_key = cur.fetchall()[0][0]
+        for j in range(len(rank_map_data[0])-1):
+            year = rank_map_data[i][j+1][0]
+            rank = rank_map_data[i][j+1][1]
+            cur.execute('INSERT INTO Rank (primary_key, year, rank, college_primary_key) VALUES ( ?, ?, ?, ? )', (None, year, rank, college_primary_key))
+
+def insert_demographic_table():
+
+    demographic_data = get_demographic_stats(demographic_base_url)
+
+    for response in demographic_data:
+        for result in response['results']:
+            sorted_keys = sorted(list(result.keys()))
+            n = 6
+            final = [sorted_keys[i * n:(i + 1) * n] for i in range((len(sorted_keys) + n - 1) // n )]
+            college_name = result[final[-1][0]]
+            cur.execute("SELECT primary_key from College WHERE college_name = ?", (college_name,))
+            primary_key = cur.fetchall()
+            if len(primary_key) > 0:
+                college_primary_key = primary_key[0][0]
+                table_year = 2013
+                for year in final[:-1]:
+                    admission_rate = result[year[0]]
+                    sat_overall = result[year[1]]
+                    loan_completion_rate = result[year[2]]
+                    median_income = result[year[3]]
+                    percent_black = result[year[4]]
+                    percent_hispanic = result[year[5]]
+                    cur.execute('INSERT INTO Demographics (primary_key, year, admission_rate, sat_overall, loan_completion_rate, percent_black, percent_hispanic, median_income, college_primary_key) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ? )', (None, table_year, admission_rate, sat_overall, loan_completion_rate, percent_black, percent_hispanic, median_income, college_primary_key))
+                    table_year += 1     
+
+if __name__ == '__main__':
+    main()
